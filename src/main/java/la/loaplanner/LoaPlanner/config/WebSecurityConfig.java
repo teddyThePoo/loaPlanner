04fc96379f31c966b02b9ade311a4b11d444c057
yjcho913 <ya1ja@naver.com>
2022-04-07 23:45:03 +0900

    refact : 주석 제거

--- src/main/java/la/loaplanner/LoaPlanner/config/WebSecurityConfig.java
+++ src/main/java/la/loaplanner/LoaPlanner/config/WebSecurityConfig.java
@@ -32,7 +32,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated() // 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
                 .and()
                 .formLogin()
-//                .loginProcessingUrl("/login")
                 .loginPage("/login") // 로그인 페이지 링크
                 .defaultSuccessUrl("/") // 로그인 성공 후 리다이렉트 주소
                 .and()
